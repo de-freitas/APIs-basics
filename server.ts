2e56cf0c0ac3277066b7de33870a76002a14972f
de-freitas <claudio.gabriel.freitas@gmail.com>
2025-10-26 21:36:18 -0300

    make `server.ts` routes connect to database

--- server.ts
+++ server.ts
@@ -1,56 +1,73 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
 import fastify from "fastify";
-import crypto from "node:crypto";
+
+import { database } from "./src/database/client";
+import { courses } from "./src/database/schema";
+import { eq } from "drizzle-orm";
 
 const server = fastify();
-
-const courses = [
-  { id: "1", title: "curso de Node.Js" },
-  { id: "2", title: "curso de React" },
-  { id: "3", title: "curso de React Native" },
-];
 
 server.get("/", (request, reply) => {
   return reply.status(200).send();
 });
 
-server.get("/courses", (request, reply) => {
-  return reply.status(200).send({ courses });
+server.get("/courses", async (request, reply) => {
+  const result = await database
+    .select({
+      id: courses.id,
+      title: courses.title,
+      descriptiom: courses.description,
+    })
+    .from(courses);
+
+  return reply.status(200).send({ courses: result });
 });
 
-server.get("/courses/:id", (request, reply) => {
+server.get("/courses/:id", async (request, reply) => {
   type Params = {
     id: string;
   };
 
   const params = request.params as Params;
-
   const courseId = params.id;
 
-  const course = courses.find((course) => course.id == courseId);
+  const result = await database
+    .select()
+    .from(courses)
+    .where(eq(courses.id, courseId));
 
-  if (course) {
-    return reply.send({ course });
+  if (result.length > 0) {
+    return reply.status(200).send({ course: result[0] });
   }
 
   return reply.status(404).send({ error: "course not found" });
 });
 
-server.post("/courses", (request, reply) => {
+server.post("/courses", async (request, reply) => {
   type RequestBody = {
     title: string;
+    description?: string;
   };
 
   const requestBody = request.body as RequestBody;
 
-  const courseId = crypto.randomUUID();
   const courseTitle = requestBody.title;
+  const courseDescription = requestBody.description;
 
   if (!courseTitle)
     return reply.status(400).send({ erro: "O título é obrigatório!" });
 
-  courses.push({ id: courseId, title: courseTitle });
+  const result = await database
+    .insert(courses)
+    .values({
+      title: courseTitle,
+      description: courseDescription,
+    })
+    .returning();
 
-  return reply.status(201).send({ courseId, courseTitle });
+  return reply.status(201).send({ courseId: result[0].id });
 });
 
 server.listen({ port: 3333 }).then(() => {
